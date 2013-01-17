00c5c1650482ca19345bea2d3549dedff6db9087
cmb69 <cmbecker69@gmx.de>
2013-01-17 19:51:05 +0000

    added German localization

---
+++ languages/de.php
@@ -0,0 +1,28 @@
+<?php
+
+$plugin_tx['chat']['user_self']="Ich";
+$plugin_tx['chat']['user_unknown']="Jemand";
+
+$plugin_tx['chat']['format_user']="{USER}: ";
+$plugin_tx['chat']['format_date']="j.n.Y";
+$plugin_tx['chat']['format_time']="H:i:s";
+
+$plugin_tx['chat']['label_send']="Senden";
+
+$plugin_tx['chat']['error_room_name']="Ungültiger Chatraum-Name: darf nur Kleinbuchstaben (a-z), Ziffern (0-9) und Bindestriche enthalten.";
+
+$plugin_tx['chat']['syscheck_title']="Systemprüfung";
+$plugin_tx['chat']['syscheck_phpversion']="PHP-Version ≥ %s";
+$plugin_tx['chat']['syscheck_extension']="Erweiterung '%s' geladen";
+$plugin_tx['chat']['syscheck_encoding']="UTF-8 Kodierung eingestellt";
+$plugin_tx['chat']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['chat']['syscheck_writable']="Ordner '%s' beschreibbar";
+
+$plugin_tx['chat']['cf_folder_data']="Pfad eines Ordners relativ zum CMSimple_XH Installationsverzeichnis, wo die Plugindaten gespeichert werden sollen. Z.B. \"userfiles/chat/\". Leer lassen, um im data/ Ordner des Plugins zu speichern.";
+$plugin_tx['chat']['cf_format_date']="Das Datumsformat wenn {DATE} in \"format user\" verwendet wird (siehe http://php.net/manual/de/function.date.php).";
+$plugin_tx['chat']['cf_format_time']="Das Zeitformat, wenn {TIME} in \"format user\" verwendet wird (siehe http://php.net/manual/de/function.date.php).";
+$plugin_tx['chat']['cf_format_user']="Das Format des Benutzernamens in der Nachricht. Folgende Platzhalter sind erlaubt: {USER} (der Benutzername), {DATE} (das Datum der Nachricht) und {TIME} (die Uhrzeit der Nachricht). Z.B.: \"{USER} schrieb am {DATE} um {TIME}: \"";
+$plugin_tx['chat']['cf_interval_poll']="Das Intervall in Sekunden zwischen dem Abrufen neuer Nachrichten.";
+$plugin_tx['chat']['cf_interval_purge']="Die Zeitdauer in Sekunden nachdem ein inaktiver Chat gelöscht wird. \"0\" bedeutet: nie löschen.";
+
+?>
--- languages/en.php
+++ languages/en.php
@@ -5,13 +5,11 @@ $plugin_tx['chat']['user_unknown']="Somebody";
 
 $plugin_tx['chat']['format_user']="{USER}: ";
 $plugin_tx['chat']['format_date']="F dS, Y";
-$plugin_tx['chat']['format_time']="H:i:s";
+$plugin_tx['chat']['format_time']="h:i:s a";
 
 $plugin_tx['chat']['label_send']="Send";
 
-$plugin_tx['chat']['error_room_name']="Invalid room name for chat()!";
-
-$plugin_tx['chat']['menu_main']="Edit Chats";
+$plugin_tx['chat']['error_room_name']="Invalid chat room name: may contain only lowercase alphanumeric letters (a-z and 0-9) and hyphens.";
 
 $plugin_tx['chat']['syscheck_title']="System check";
 $plugin_tx['chat']['syscheck_phpversion']="PHP version ≥ %s";
@@ -20,11 +18,11 @@ $plugin_tx['chat']['syscheck_encoding']="Encoding 'UTF-8' configured";
 $plugin_tx['chat']['syscheck_magic_quotes']="Magic quotes runtime off";
 $plugin_tx['chat']['syscheck_writable']="Folder '%s' writable";
 
-$plugin_tx['chat']['cf_folder_data']="Path to a folder relative to the CMSimple root directory, where to store the plugin's data. E.g. <em>userfiles/chat/</em>. Leave empty to store into the plugin's data/ folder.";
+$plugin_tx['chat']['cf_folder_data']="Path of a folder relative to the CMSimple_XH root directory, where to store the plugin's data. E.g. \"userfiles/chat/\". Leave empty to store into the plugin's data/ folder.";
 $plugin_tx['chat']['cf_format_date']="The format of the date when used as {DATE} in \"format user\" (see http://php.net/manual/en/function.date.php).";
 $plugin_tx['chat']['cf_format_time']="The format of the time when used as {TIME} in \"format user\" (see http://php.net/manual/en/function.date.php).";
-$plugin_tx['chat']['cf_format_user']="The format of the username (and optionally the date and time of posting) in the message. The following placeholders are allowed: {USER} (the name of the user), {DATE} (the date of posting) and {TIME} (the time of posting). E.g.: \"{USER} wrote on {DATE} at {TIME}: \"";
-$plugin_tx['chat']['cf_interval_poll']="The time in seconds between the next poll for new messages.";
-$plugin_tx['chat']['cf_interval_purge']="The time in seconds after which an inactive chat will be purged. \"0\" means keep entries forever.";
+$plugin_tx['chat']['cf_format_user']="The format of the username in the message. The following placeholders are allowed: {USER} (the name of the user), {DATE} (the date of posting) and {TIME} (the time of posting). E.g.: \"{USER} wrote on {DATE} at {TIME}: \"";
+$plugin_tx['chat']['cf_interval_poll']="The interval in seconds between the next poll for new messages.";
+$plugin_tx['chat']['cf_interval_purge']="The time in seconds after which an inactive chat will be purged. \"0\" means: keep entries forever.";
 
 ?>
