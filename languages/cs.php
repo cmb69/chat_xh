c48e3d0bf316e686f5838073f42e1f62b1fded9d
cmb69 <cmbecker69@gmx.de>
2013-01-17 19:59:46 +0000

    added Czech and Slovak localization (contributed by oldnema resp. Tata)

---
+++ languages/cs.php
@@ -0,0 +1,18 @@
+<?php
+
+$plugin_tx['chat']['user_self']="Já";
+$plugin_tx['chat']['user_unknown']="Anonym";
+$plugin_tx['chat']['label_send']="Odeslat";
+$plugin_tx['chat']['error_room_name']="Nesprávný název místnosti pro chat()!";
+$plugin_tx['chat']['syscheck_title']="Kontrola systému";
+$plugin_tx['chat']['syscheck_phpversion']="PHP verze ≥ %s";
+$plugin_tx['chat']['syscheck_extension']="Rozšíření '%s' načteno";
+$plugin_tx['chat']['syscheck_encoding']="Kódování 'UTF-8' nastaveno";
+$plugin_tx['chat']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['chat']['syscheck_writable']="Do adresáře '%s' lze zapisovat.";
+$plugin_tx['chat']['cf_folder_data']="Cesta k adresáři relativní ke kořenovému adresáři CMSimple_XH, kam se mají ukládat data pluginu. Např.: \"userfiles/chat/\". Pokut nic nezadáte, budou se data zapisovat do adresáře pluginu /data.";
+$plugin_tx['chat']['cf_format_user']="Formát uživatelského jména ve zprávě. {USER} je znak pro umístění uživatelského jména.";
+$plugin_tx['chat']['cf_interval_poll']="Čas v sekundách pro novou zprávu.";
+$plugin_tx['chat']['cf_interval_purge']="Čas v sekundách, po kterém bude chat vyčištěn.";
+
+?>
---
+++ languages/sk.php
@@ -0,0 +1,18 @@
+<?php
+
+$plugin_tx['chat']['user_self']="Ja";
+$plugin_tx['chat']['user_unknown']="Niekto";
+$plugin_tx['chat']['label_send']="Odoslať";
+$plugin_tx['chat']['error_room_name']="Nesprávny názov miestnosti pre chat()!";
+$plugin_tx['chat']['syscheck_title']="Kontrola systému";
+$plugin_tx['chat']['syscheck_phpversion']="PHP verzia ≥ %s";
+$plugin_tx['chat']['syscheck_extension']="Rozšírenie '%s' načítané";
+$plugin_tx['chat']['syscheck_encoding']="Kódovanie 'UTF-8' nastavené";
+$plugin_tx['chat']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['chat']['syscheck_writable']="Do adresára '%s' možno zapisovať.";
+$plugin_tx['chat']['cf_folder_data']="Cesta k adresáru relatívna ku koreňovému adresáru CMSimple_XH, kam sa majú ukladať dáta pluginu. Napr.: \"userfiles/chat/\". Ak nič neuvediete, budú sa dáta zapisovať do adresára pluginu /data.";
+$plugin_tx['chat']['cf_format_user']="Formát užívateľského mena v správe. {USER} je znak pre umiestnenie uživateľského mena.";
+$plugin_tx['chat']['cf_interval_poll']="Čas v sekundách pre novú správu.";
+$plugin_tx['chat']['cf_interval_purge']="Čas v sekundách, po ktorom bude chat vyčistený.";
+
+?>
