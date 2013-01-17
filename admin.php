be42e17d4672a97b72536fba6054753a835902ff
cmb69 <cmbecker69@gmx.de>
2013-01-17 18:11:11 +0000

    refactored about information as view

--- admin.php
+++ admin.php
@@ -26,20 +26,15 @@ if (!defined('CMSIMPLE_XH_VERSION')) {
  */
 function Chat_about()
 {
-    return '<h1><a href="http://3-magi.net/?CMSimple_XH/Chat_XH">Chat_XH</a></h1>'
-	. '<p>Version: ' . CHAT_VERSION . '</p>'
-	. '<p>Copyright &copy; 2012-2013 <a href="http://3-magi.net/">Christoph M. Becker</a></p>'
-	. '<p style="text-align: justify">This program is free software: you can redistribute it and/or modify'
-	. ' it under the terms of the GNU General Public License as published by'
-	. ' the Free Software Foundation, either version 3 of the License, or'
-	. ' (at your option) any later version.</p>'
-	. '<p style="text-align: justify">This program is distributed in the hope that it will be useful,'
-	. ' but WITHOUT ANY WARRANTY; without even the implied warranty of'
-	. ' MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the'
-	. ' GNU General Public License for more details.</p>'
-	. '<p style="text-align: justify">You should have received a copy of the GNU General Public License'
-	. ' along with this program.  If not, see'
-	. ' <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>';
+    global $pth;
+
+    $icon = tag('img src="' . $pth['folder']['plugins']
+		. 'chat/chat.png" alt="Plugin Icon"');
+    $bag = array('heading' => 'Chat_XH',
+		 'url' => 'http://3-magi.net/?CMSimple_XH/Chat_XH',
+		 'icon' => $icon,
+		 'version' => CHAT_VERSION);
+    return Chat_view('about', $bag);
 }
 
 
