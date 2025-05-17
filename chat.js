791862babc34fa516f2f1112df25c6628f77e9ba
Christoph M. Becker <cmbecker69@gmx.de>
2025-05-17 17:28:32 +0200

    Revise JS a bit
    Wow, time flies!

--- chat.js
+++ chat.js
@@ -5,34 +5,10 @@
  * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
  */
 
-/*jslint browser: true, maxlen: 80*/
 /*global CHAT*/
 
 (function () {
     "use strict";
-
-    /**
-     * Calls a function for each element with the given class.
-     *
-     * @param {string}   className A class name.
-     * @param {function} func      A callback function.
-     *
-     * @returns {undefined}
-     */
-    function doForEach(className, func) {
-        var elements, i, n;
-
-        if (typeof document.getElementsByClassName !== "undefined") {
-            elements = document.getElementsByClassName(className);
-        } else if (typeof document.querySelectorAll !== "undefined") {
-            elements = document.querySelectorAll("." + className);
-        } else {
-            elements = [];
-        }
-        for (i = 0, n = elements.length; i < n; i += 1) {
-            func(elements[i]);
-        }
-    }
 
     /**
      * Registers a load event listener for window.
@@ -86,8 +62,7 @@
             var request;
 
             function mustScroll() {
-                return messages.scrollTop >=
-                    messages.scrollHeight - messages.clientHeight;
+                return messages.scrollTop >= messages.scrollHeight - messages.clientHeight;
             }
 
             function onReadyStateChange() {
@@ -123,10 +98,7 @@
 
             request = new XMLHttpRequest();
             request.open("POST", url + "write");
-            request.setRequestHeader(
-                "Content-Type",
-                "application/x-www-form-urlencoded"
-            );
+            request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
             request.onreadystatechange = onReadyStateChange;
             msg = form.elements.chat_message.value;
             request.send("chat_message=" + encodeURIComponent(msg));
@@ -135,19 +107,15 @@
 
         room = element.getAttribute("data-chat-room");
         url = CHAT.url + "&chat_room=" + room + "&chat_ajax=";
-        messages = document.getElementById(
-            "chat_room_" + room + "_messages"
-        );
-        form = document.getElementById("chat_room_" + room + "_form");
+        messages = document.querySelector("#chat_room_" + room + "_messages");
+        form = document.querySelector("#chat_room_" + room + "_form");
         scrollDown();
-        if (typeof XMLHttpRequest !== "undefined") {
-            form.onsubmit = submit;
-            setInterval(poll, CHAT.interval);
-        }
+        form.onsubmit = submit;
+        setInterval(poll, CHAT.interval);
     }
 
     onload(function () {
-        doForEach("chat_room", function (element) {
+        document.querySelectorAll(".chat_room", function (element) {
             initWidget(element);
         });
     });
