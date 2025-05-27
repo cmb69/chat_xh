055ee905505d3adee6faddae0191c592b6f4d359
Christoph M. Becker <cmbecker69@gmx.de>
2025-05-27 16:50:04 +0200

    Drop unused magic_quotes language strings

--- languages/sk.php
+++ languages/sk.php
@@ -6,7 +6,6 @@ $plugin_tx['chat']['label_send'] = "Odoslať";
 $plugin_tx['chat']['error_room_name'] = "Nesprávny názov miestnosti pre chat()!";
 $plugin_tx['chat']['syscheck_title'] = "Kontrola systému";
 $plugin_tx['chat']['syscheck_phpversion'] = "PHP verzia ≥ %s";
-$plugin_tx['chat']['syscheck_magic_quotes'] = "Magic quotes runtime off";
 $plugin_tx['chat']['syscheck_writable'] = "Do adresára '%s' možno zapisovať.";
 $plugin_tx['chat']['cf_interval_poll'] = "Čas v sekundách pre novú správu.";
 $plugin_tx['chat']['cf_interval_purge'] = "Čas v sekundách, po ktorom bude chat vyčistený.";
